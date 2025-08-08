759ed4b4f8f7295952a65ebe6f435990e1ad6b1c
vyxblvs <newdiscord69420@proton.me>
2025-08-07 18:00:29 -0700

    ignore

--- WinLoad64/kernelbase.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "pch.h"
-
-// Invalid bits for dwFlags in LoadLibrary
-#define LOAD_LIBRARY_INVALID_BITS 0xFFFF000
-
-// Invalid flag combo for dwFlags in LoadLibrary
-#define LOAD_LIBRARY_DATAFILE_BOTH LOAD_LIBRARY_AS_DATAFILE | LOAD_LIBRARY_AS_DATAFILE_EXCLUSIVE
